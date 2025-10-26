2c5caa7218f7eb1d1d34945a77c37257a216d0a8
Rodrique09 <ddtxrodrique@gmail.com>
2025-10-26 18:09:26 -0500

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ function App() {
     <div className="App">
       <nav className="navbar">
         <h1>🎬 Movie Watchlist</h1>
+        <div className="nav-links">
+          <Link to = "/">Search</Link>
+          <Link to = "/watchlist">My Watchlist</Link>
+        </div>
       </nav>
+
+      <Routes>
+        <Route path = "/" element = {<div>Search Page</div>} />
+        <Route path = "/watchlist" element = {<div>Watchlist Page</div>} />
+        <Route path = "/movie/:id" element = {<div>Movie Details Page</div>} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+
     </div>
   )
 }
 
-export default App
+export default App;
