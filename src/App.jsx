c89f1ee4b44f868d1d5e02e12d117f49a556ee32
Rodrique09 <ddtxrodrique@gmail.com>
2025-10-26 17:50:05 -0500

    Project Start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route, Link} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <h1>🎬 Movie Watchlist</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default App
